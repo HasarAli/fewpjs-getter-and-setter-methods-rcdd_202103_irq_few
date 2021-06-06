9f99f25b30c1dd1af6e14f7ab4dfff506c098b72
Hasar Ali <hasar.ali@protonmail.com>
2021-06-06 01:11:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(raduis) {
+    this.raduis = raduis
+  }
+  
+  get diameter() {
+    return this.raduis * 2
+  }
+  
+  get circumference() {
+    
+  }
+}
