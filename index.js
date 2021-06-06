3133465dab5c9254d170254cc5ef643ae79a65c4
Hasar Ali <hasar.ali@protonmail.com>
2021-06-06 01:15:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ class Circle {
   }
   
   get circumference() {
-    
+    return this.raduis * 2 * Math.PI 
+  }
+  
+  get area() {
+    return Math.PI * this.raduis * this.raduis
+  }
+  
+  set diameter(d) {
+    this.raduis = d/2
+  }
+  
+  set circumference(c) {
+    this.raduis = c/(2*Math.PI)
+  }
+  
+  set area(a) {
+    this.raduis = 
   }
 }
